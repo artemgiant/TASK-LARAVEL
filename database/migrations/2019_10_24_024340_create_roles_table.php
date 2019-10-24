2e0c33cbd98f810cd1fddade7df4b76ea090ce73
artem <artemgiant@gamil.com>
2019-10-24 04:47:59 +0200

    fix

--- database/migrations/2019_10_24_024340_create_roles_table.php
+++ database/migrations/2019_10_24_024340_create_roles_table.php
@@ -11,21 +11,17 @@ class CreateRolesTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up ()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+        Schema :: create ('role', function (Blueprint $table) {
+        $table-> increments ('id');
+    $table-> string ('name');
+    $table-> string ( 'description');
+    $table-> timestamps ();
+  });
+}
+    public function down ()
     {
-        Schema::dropIfExists('roles');
+        Schema :: dropIfExists ('role');
     }
 }
